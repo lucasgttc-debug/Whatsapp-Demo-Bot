19d7d362a50975697e12efa3513512541563571e
lucasgttc-debug <lucasgttc@gmail.com>
2025-11-26 07:44:54 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -36,6 +36,7 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Bot corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Bot corriendo en puerto " + PORT);
 });
