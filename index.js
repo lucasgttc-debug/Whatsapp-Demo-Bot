51ff3c711ad6be1d03a0f1ee8a249148a38f962e
lucasgttc-debug <lucasgttc@gmail.com>
2025-11-28 10:46:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import fetch from "node-fetch";
 
 const app = express();
 app.use(bodyParser.json());
+
+// ⬇️ REEMPLAZAR ESTOS VALORES
 const VERIFY_TOKEN = "lucasbot25";
-const ACCESS_TOKEN = "EAAhkFIOiHCwBQD1Xxd6YNK1EdYZAh1wagBISJJOnRzrZAZAyE9Q8mkV52j4ZCaOIX0obFUKBeoULJvOZAxxGZCC2oZAe0rINzZCb1eZABl3JjD9uTrtpjV7LHm7eDWXPYZAw9M2XTfZCgLzbKEbOgRNaJwi0GAZApZCm0oTXIceJWjGUbN9J9eIL5yU8kvnk8vZCuxVid49sAH9q9CKqnyojEJ9eigws2yk3kGtgMXozM94s3ynmgiEHphNAZDZD";
+const ACCESS_TOKEN = "EAAhkFIOiHCwBQLPQ5vDu9C7qbWrl4dbvN6w0Ry5o5b0GjdAvV5dWxfn17wz684QCP5RiccquzLPK7fpCZAbBD03hZCWPDLzfS4Ts7d3E3xZCZC2k3csRo7rePb4UZC2izHB5ZBYZBZBj030HBlSNILwJd7umRiJbnDknEU8cdq0xvmlBDVGidchfUw40Mp5dlsNq0tz7HCnhcZClwfL09EMuw8TqBqj5ZAIX44QMd35rcLzMyMmFeSfAZDZD";
 const PHONE_NUMBER_ID = "860037433865539";
 
 app.get("/webhook", (req, res) => {
@@ -22,7 +24,8 @@ app.get("/webhook", (req, res) => {
 
 app.post("/webhook", async (req, res) => {
   try {
-    const entry = req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
+    const entry =
+      req.body.entry?.[0]?.changes?.[0]?.value?.messages?.[0];
 
     if (entry && entry.type === "text") {
       const from = entry.from;
@@ -41,8 +44,8 @@ app.post("/webhook", async (req, res) => {
           body: JSON.stringify({
             messaging_product: "whatsapp",
             to: from,
-            text: { body: "Mensaje recibido: " + message },
-          }),
+            text: { body: "Mensaje recibido: " + message }
+          })
         }
       );
     }
